143a2f2e14124afba9b1369b802afad117c91e4b
taire <git@taire.de>
2022-05-26 22:49:32 +0200

    init woop woop

---
+++ setwacompics.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# upper half
+intuos4oled.py set -b 0 -i arrow-undo.png
+intuos4oled.py set -b 1 -i arrow-redo.png
+intuos4oled.py set -b 2 -i erase.png
+intuos4oled.py set -b 3 -i mirror.png
+# lower half
+intuos4oled.py set -b 4 -i nextkeyframe.png
+intuos4oled.py set -b 5 -i colorhistory.png
+intuos4oled.py set -b 6 -i colorpalette.png
+intuos4oled.py set -b 7 -i save.png
+
